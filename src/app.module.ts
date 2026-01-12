6117f2dc03d3de8b0cbef913e0ecb2aaade88b84
shahd <eng.shahd.abusharif@gmail.com>
2026-01-12 10:15:53 -0800

    modifying imports and adding AuthModule and UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 //? this is the Root module
-
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [], // any module we make , we should import it here
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, DatabaseModule], // any module we make , we should import it here
+
 })
 export class  AppModule {}
